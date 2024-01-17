fe1cf16b3140cfebd4b0f03638e827c8dacfe3bc
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-01-17 21:05:49 +0530

    Horizontal Navbar UI Created

--- app/HorizontalNavBar.tsx
+++ app/HorizontalNavBar.tsx
@@ -1,14 +1,24 @@
-import { Flex } from "@radix-ui/themes";
+import { Avatar, Flex, Heading, Text } from "@radix-ui/themes";
 import React from "react";
 
 const HorizontalNavBar = () => {
   return (
     <Flex
       className="h-[70px] w-full border shadow-md rounded-lg"
+      px={"5"}
       align={"center"}
-      justify={"center"}
+      justify={"between"}
     >
-      HorizontalNavBar
+      <Flex>{/* <Heading size={"5"}></Heading> */}</Flex>
+      <Flex gap={"3"} align={"center"}>
+        <Flex direction={"column"} align={"end"}>
+          <Heading size={"2"}>Imtiyaz Sayyid</Heading>
+          <Text className="text-sm text-slate-500">Admin</Text>
+        </Flex>
+        <Flex>
+          <Avatar fallback={"?"} />
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
