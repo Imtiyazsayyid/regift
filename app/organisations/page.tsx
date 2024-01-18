c297c39061677039044da0666436e4ff42f9331c
FahadKhan99 <fahad.khan44604@gmail.com>
2024-01-18 23:47:23 +0530

    "Fixed og state"

--- app/organisations/page.tsx
+++ app/organisations/page.tsx
@@ -1,18 +1,7 @@
-'use client'
-import { useEffect } from 'react';
 import { Flex } from "@radix-ui/themes";
 import React from "react";
-import axios from "axios";
 
 const OrganisationPage = () => {
-  const getOrgs = async () => {
-    const res = await axios.get("http://localhost:3000/organisation");
-    console.log(res);
-  }
- useEffect(() => {
-  getOrgs();
- }, [])
-
   return <div>Organisation</div>
 };
 
