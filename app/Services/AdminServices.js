05da24667a8366b926eb826f6bccd3e893b0380d
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-01-19 00:07:04 +0530

    refactoring admin apis

--- app/Services/AdminServices.js
+++ app/Services/AdminServices.js
@@ -22,20 +22,20 @@ export async function getAllOrganisations(params) {
 }
 
 export async function getSingleOrganisation() {
-  return await Api.get("/organisations/:id");
+  return await Api.get("/organisation/:id");
 }
 
 export async function saveOrganisation(payload) {
-  return await Api.post("/organisations", payload);
+  return await Api.post("/organisation", payload);
 }
 
 export async function deleteOrganisation() {
-  return await Api.delete("/organisations/:id");
+  return await Api.delete("/organisation/:id");
 }
 
 //Inventory
 export async function getAllInventories(params) {
-  return await Api.get("/inventory", { params });
+  return await Api.get("/inventories", { params });
 }
 
 export async function getSingleInventory() {
@@ -43,7 +43,7 @@ export async function getSingleInventory() {
 }
 
 export async function saveInventory(payload) {
-  return await Api.post("/inventory", payload);
+  return await Api.post("/inventories", payload);
 }
 
 export async function deleteInventory() {
