753ebb7ad4815d7e348669eb3c5bd097d6426bfe
FahadKhan99 <fahad.khan44604@gmail.com>
2024-01-18 23:46:21 +0530

    "Added Api"

--- app/Services/AdminServices.js
+++ app/Services/AdminServices.js
@@ -1,5 +1,6 @@
 import Api from "./Api";
 
+//User
 export async function getAllUsers(params) {
   return await Api.get("/users", { params });
 }
@@ -14,4 +15,38 @@ export async function saveUser(payload) {
 
 export async function deleteUser() {
   return await Api.delete("/users/:id");
+}
+
+//Organisation
+export async function getAllOrganisations(params) {
+  return await Api.get("/organisations", { params });
+}
+
+export async function getSingleOrganisation() {
+  return await Api.get("/organisations/:id");
+}
+
+export async function saveOrganisation(payload) {
+  return await Api.post("/organisations", payload);
+}
+
+export async function deleteOrganisation() {
+  return await Api.delete("/organisations/:id");
+}
+
+//Inventory
+export async function getAllInventories(params) {
+  return await Api.get("/inventory", { params });
+}
+
+export async function getSingleInventory() {
+  return await Api.get("/inventory/:id");
+}
+
+export async function saveInventory(payload) {
+  return await Api.post("/inventory", payload);
+}
+
+export async function deleteInventory() {
+  return await Api.delete("/inventory/:id");
 }
