476999bb32c4b1b5105b7e5cef5980ce630eb5c8
FahadKhan99 <fahad.khan44604@gmail.com>
2024-01-18 20:14:14 +0530

    "testing"

--- app/organisations/page.tsx
+++ app/organisations/page.tsx
@@ -1,7 +1,19 @@
+'use client'
+import { useEffect } from 'react';
+import { Flex } from "@radix-ui/themes";
 import React from "react";
+import axios from "axios";
 
 const OrganisationPage = () => {
-  return <div>Organisations Page</div>;
+  const getOrgs = async () => {
+    const res = await axios.get("http://localhost:3000/organisation");
+    console.log(res);
+  }
+ useEffect(() => {
+  getOrgs();
+ }, [])
+
+  return <div>Organisation</div>
 };
 
 export default OrganisationPage;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,16 @@
+'use client'
 import React from "react";
+import axios from 'axios';
+import {useEffect} from 'react';
 
 const UsersPage = () => {
+  const getUser = async () => {
+    const res = await axios.get("http://localhost:3000/users");
+    console.log(res.data.data);
+  }
+  useEffect(()=> {
+    getUser();
+  },[])
   return <div>Users Page</div>;
 };
 
