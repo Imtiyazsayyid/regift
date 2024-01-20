4303409710b5461a77dc55a4025f04eb4cd45b47
FahadKhan99 <fahad.khan44604@gmail.com>
2024-01-20 19:04:38 +0530

    Add category & DonatedItem

--- app/Services/AdminServices.js
+++ app/Services/AdminServices.js
@@ -1,5 +1,6 @@
 import Api from "./Api";
 
+// Donor
 export async function getAllDonors(params) {
   return await Api.get("/donors", { params });
 }
@@ -21,16 +22,16 @@ export async function getAllOrganisations(params) {
   return await Api.get("/organisations", { params });
 }
 
-export async function getSingleOrganisation() {
-  return await Api.get("/organisation/:id");
+export async function getSingleOrganisation(payload) {
+  return await Api.get(`/organisation/${payload.id}`);
 }
 
 export async function saveOrganisation(payload) {
-  return await Api.post("/organisation", payload);
+  return await Api.post("/organisations", payload);
 }
 
-export async function deleteOrganisation() {
-  return await Api.delete("/organisation/:id");
+export async function deleteOrganisation(id) {
+  return await Api.delete(`/organisation/${id}`);
 }
 
 //Inventory
@@ -38,14 +39,49 @@ export async function getAllInventories(params) {
   return await Api.get("/inventories", { params });
 }
 
-export async function getSingleInventory() {
-  return await Api.get("/inventory/:id");
+export async function getSingleInventory(payload) {
+  return await Api.get(`/inventory/${payload.id}`);
 }
 
 export async function saveInventory(payload) {
   return await Api.post("/inventories", payload);
 }
 
-export async function deleteInventory() {
-  return await Api.delete("/inventory/:id");
+export async function deleteInventory(id) {
+  return await Api.delete(`/inventory/${id}`);
 }
+
+// Category
+export async function getAllCategories(params) {
+  return await Api.get("/categories", { params });
+}
+
+export async function getSingleCategory(payload) {
+  return await Api.get(`/category/${payload.id}`);
+}
+
+export async function saveCategory(payload) {
+  return await Api.post("/categories", payload);
+}
+
+export async function deleteCategory(id) {
+  return await Api.delete(`/category/${id}`);
+}
+
+// Donated Item
+export async function getAllDonatedItems(params) {
+  return await Api.get("/donated-items", { params });
+}
+
+export async function getSingleDonatedItem(payload) {
+  return await Api.get(`/donated-item/${payload.id}`);
+}
+
+export async function deleteDonatedItem(id) {
+  return await Api.delete(`/donated-item/${id}`);
+}
+
+
+
+
+
