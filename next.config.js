b4486d7119f6e7f09d066b9d9fdb781f3f83647d
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-01-24 01:11:29 +0530

    Final Next Config Hopefully

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   env: {
     // BASE_URL: "http://localhost:8003/api/admin",
-    BASE_URL: "https://regift-backend.vercel.app/api/admin",
+    // BASE_URL: "https://regift-backend.vercel.app/api/admin",
+    BASE_URL:
+      process.env.NODE_ENV === "production"
+        ? "https://regift-backend.vercel.app/api/admin"
+        : "http://localhost:8003/api/admin",
   },
 };
 
