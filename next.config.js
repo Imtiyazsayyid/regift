0c89d4b6d85aecc71cfc0f562e8e4dfcb2c4d842
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-01-24 01:03:41 +0530

    Fix Admin Details Bug

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // BASE_URL: "http://localhost:8003/api/admin",
-    BASE_URL: "https://regift-backend.vercel.app/api/admin",
+    BASE_URL: "http://localhost:8003/api/admin",
+    // BASE_URL: "https://regift-backend.vercel.app/api/admin",
   },
 };
 
