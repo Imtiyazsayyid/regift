179e0ca7f9651c39ed177dc3106332b7c8eb2235
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-01-24 22:47:09 +0530

    Branches Test

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -11,8 +11,7 @@ export default function Home() {
         gap={"4"}
       >
         <Heading size={"9"}>
-          Welcome To <span className="text-blue-600">ReGift</span>{" "}
-          Administrator.
+          Welcome To <span className="text-blue-600">ReGift</span> Admin.
         </Heading>
         <Text className="w-1/4 text-slate-600" align={"center"}>
           The place where you can donate freely while being assured it goes to
